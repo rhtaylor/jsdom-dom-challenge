977a1faa38338a874bd61c1764d8ef6cab1f3162
Randy <randyisbusy@gmail.com>
2020-02-10 13:24:44 -0700

    counter function functional

--- js/challenge.js
+++ js/challenge.js
@@ -5,19 +5,21 @@ document.addEventListener("DOMContentLoaded", function(){
 function initializeCounter(){ 
 const counter = document.getElementById("counter") 
 let numberOfCount = counter.innerText
-let time = parseInt(numberOfCount)  
+let time = parseInt(numberOfCount, 10)  
 startCount(time)
 }
 
-function startCount(time){ 
+function startCount(time){  
+    
     counter.innerHTML = time  
      timedCount(time) 
-    debugger
+    
 }
 
 function timedCount(time){
-    //=> worked till 2 then got NaN. Looking at the scope chain I do not know why???
+    //=> worked till 2 then got NaN. Looking at the scope chain I do not know why??? 
+    debugger
     time++
     setTimeout(function () { startCount(time) }, 1000); 
-    t = setTimeout(timedCount, 1000);
+   // t = setTimeout(timedCount, 1000);
 }
