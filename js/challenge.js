b3f7e632213a877494c05e8faff495df97097d49
Randy <randyisbusy@gmail.com>
2020-02-12 13:11:01 -0700

    changed to setInterval from setTimeout

--- js/challenge.js
+++ js/challenge.js
@@ -8,45 +8,36 @@ const add1Button = document.getElementById("plus");
 const minusButton = document.getElementById("minus"); 
 const heartButton = document.getElementById("heart");
 const pauseButton = document.getElementById("pause");
-let numberOfCount = counter.innerText;
+let numberOfCount = counter.innerText; 
+console.log(numberOfCount)
 let time = parseInt(numberOfCount, 10);  
-startCount(time)
-changeCounterDom()
-    function changeCounterDom(time) {
-        add1Button.addEventListener("click", () => { increaseCounter(event, time) }) ;
-        minusButton.onclick =  increaseCounter(event, time); 
-             numberOfCount = time
-        heartButton.onclick = function () { console.log("Heart button pushed") }
+startCount(time) 
+
+    console.log("im in initiazize", time)
+        add1Button.addEventListener("click", increaseCounter) ; 
         
-    }
-
-}
-
-function startCount(time){  
-    
+        //add1Button.onclick = increaseCounter(event, time); 
+ 
+function startCount(){  
     counter.innerHTML = time  
-     timedCount(time) 
+     timedCount() 
     
 } 
 
-function timedCount(time){
-    time++ 
-    console.log(time)
-    setTimeout(function () { startCount(time) }, 4000); 
+function timedCount(){
+    console.log("im in timedCount", time)
+    setInterval(function () { increaseCounter() }, 5000); 
    // t = setTimeout(timedCount, 1000);
 } 
 
          
-function increaseCounter(event, time){  
-console.log(event, "Should be time//=>", time)  
-debugger
-   ++time  
-   console.log(time)
-   numberOfCount = time  
+function increaseCounter() {    
+    ++time  
+   console.log("im in increaseCounter", time)
+    counter.innerHTML = time   
    
    
 } 
 
-function subtract(time){
-    console.log(time)
+
 }
