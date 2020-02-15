59c4e82c9f0d625ef14b4d9b80428d7c38b028fe
Randy <randyisbusy@gmail.com>
2020-02-15 02:39:57 -0700

    like functionality working with heart button

--- js/challenge.js
+++ js/challenge.js
@@ -2,13 +2,18 @@ document.addEventListener("DOMContentLoaded", function(){
     initializeCounter()
 });
 
-function initializeCounter(){ 
+function initializeCounter(){  
+let i = 0;
+
 const counter = document.getElementById("counter"); 
 const add1Button = document.getElementById("plus");
 const minusButton = document.getElementById("minus"); 
 const heartButton = document.getElementById("heart");
 const pauseButton = document.getElementById("pause");
-let numberOfCount = counter.innerText; 
+const likeBox = document.querySelector(".likes");
+
+let numberOfCount = counter.innerText;  
+
 console.log(numberOfCount)
 let time = parseInt(numberOfCount, 10);  
 startCount(time) 
@@ -18,44 +23,61 @@ startCount(time)
         minusButton.addEventListener("click", decrementCounter);
         heartButton.addEventListener("click", heartNum); 
  
-function startCount(){  
-    counter.innerHTML = time  
-     timedCount() 
+    function startCount(){  
+        counter.innerHTML = time  
+        timedCount() 
          
 } 
 
-function timedCount(){
-    console.log("im in timedCount", time)
-    setInterval(function () { increaseCounter() }, 5000); 
-   // t = setTimeout(timedCount, 1000);
+    function timedCount(){
+        console.log("im in timedCount", time)
+        setInterval(function () { increaseCounter() }, 20000); 
+  
 } 
 
          
-function increaseCounter() {    
-    ++time
-    i = 0
-   console.log("im in increaseCounter", time)
-    counter.innerHTML = time   
+    function increaseCounter() {    
+        ++time;
+         i = 0;
+    console.log("I control the count, im in increaseCounter", time, "i //=>", i)
+        counter.innerHTML = time   
    
    
 } 
 
-function decrementCounter(){
-    --time 
-    counter.innerHTML = time
-    console.log("counter -1", time) 
-    debugger
+    function decrementCounter(){
+        --time 
+        counter.innerHTML = time
+        console.log("counter -1", time) 
+        debugger
 }
 
 
-let arry = [] 
-let i = 0 
-function heartNum(e){  
+let arryOfLi = [] 
 
-    arry.push(time, "pushed", i)
-   ++i 
-   console.log(arry) 
+        function heartNum(e){
+                if (e){ i++}
+            let likesDiv =  document.querySelector(".likes")
+            console.log(likesDiv) 
+                // stoping the flow of logic to prevent duplcate li for same number    
+            let liAlreadyMade = document.querySelector(`.li${time}`); 
+            
+            console.log("let logic flow ", liAlreadyMade)
+            if (liAlreadyMade) { 
+                
+                liAlreadyMade.innerText = `${time} liked ${i} times` 
+                console.log("something not working", i)
+            } else {
+                
+                let liForDiv = document.createElement("li");
+             
+            liForDiv.setAttribute("class", `li${time}`); 
+            likesDiv.appendChild(liForDiv); 
+            let LisInDiv =  document.querySelector(`.li${time}`); 
+            arryOfLi.push(LisInDiv);
+            liForDiv.innerHTML = `${time} liked ${i} times`;
+            }
+        }
 
-}
 
 }
